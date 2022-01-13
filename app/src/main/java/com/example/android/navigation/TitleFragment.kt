2121e2252d1a003c9b67ba907dcee83a1a8215a7
Isaac Miti <ikayz@live.com>
2022-01-13 23:03:30 +0200

    Add binding to inflate the layout

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.android.navigation.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
     override fun onCreateView(
@@ -12,6 +14,9 @@ class TitleFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_title, container, false)
+//        return inflater.inflate(R.layout.fragment_title, container, false)
+        val binding: FragmentTitleBinding = DataBindingUtil.inflate(inflater,
+        R.layout.fragment_title, container, false)
+        return binding.root
     }
 }
