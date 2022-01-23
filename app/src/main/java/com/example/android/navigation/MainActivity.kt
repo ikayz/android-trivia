d1f6df67ff0369eeb60bb5ca81e634df854993da
Isaac Miti <ikayz@live.com>
2022-01-23 23:38:25 +0200

    Add the about fragment

--- app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onSupportNavigateUp(): Boolean {
-
+        val navController = this.findNavController(R.id.myNavHostFragment)
         return super.onSupportNavigateUp()
     }
 }
